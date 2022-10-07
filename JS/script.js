44f36babd0a65760d9025a0be64c15abca970787
FernandezIgnacio <fernandez.ignacio.epet20@gmail.com>
2022-10-07 18:43:15 -0300

    Escrito formulario del punto diez

---
+++ JS/script.js
@@ -0,0 +1,10 @@
+var datos = function (){
+    var numero = document.getElementById("numero").value;
+    console.log(numero)
+    var validacion = document.getElementById("validacion").value;
+    if (validacion=17){
+        console.log("No es un robot.")
+    } else {
+        console.log("Es un robot.")
+    }
+}
