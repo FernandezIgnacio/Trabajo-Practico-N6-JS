707a0ec6301b14c3c45e30da605f72f0b9b101f6
FernandezIgnacio <fernandez.ignacio.epet20@gmail.com>
2022-11-07 12:04:43 -0300

    Añadido los comentarios

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,9 @@
 var datos = function (){
-    var numero = document.getElementById("numero").value;
-    console.log(numero)
-    var validacion = document.getElementById("validacion").value;
-    if (validacion=17){
-        console.log("No es un robot.")
+    var numero = document.getElementById("numero").value; //Recibe los datos del html
+    console.log(numero) //Muestra el numero por consola
+    var validacion = document.getElementById("validacion").value; //Recibe los datos del html
+    if (validacion=17){ //Verifica que el resultado sea valido, y devuelve la validacion por consola
+        console.log("No es un robot.") 
     } else {
         console.log("Es un robot.")
     }
